2c909a7607d362680f7edd0bd9f123f90d108904
msuzer <msuzer1@gmail.com>
2026-01-07 15:48:45 +0300

    refactor: simplify MuxMotion initialization

--- gasera/motion/mux_motion.py
+++ gasera/motion/mux_motion.py
@@ -1,11 +1,23 @@
 # motion/mux_motion.py
+from system.mux.cascaded_mux import CascadedMux
 from system.mux.mux_gpio import GPIOMux
 from system.mux.mux_vici_uma import ViciUMAMux
+from system.gpio import pin_assignments as PINS
 
 class MuxMotion:
-    def __init__(self, cmux_gpio: GPIOMux, cmux_serial: ViciUMAMux):
-        self.cmux_gpio = cmux_gpio
-        self.cmux_serial = cmux_serial
+    def __init__(self):
+        serial_port1 = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A90KFA3G-if00-port0"
+        serial_port2 = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A9C7BUGI-if00-port0"
+
+        self.cmux_gpio = CascadedMux(
+            GPIOMux(home_pin=PINS.OC5_PIN, next_pin=PINS.OC4_PIN),
+            GPIOMux(home_pin=PINS.OC2_PIN, next_pin=PINS.OC1_PIN),
+        )
+
+        self.cmux_serial = CascadedMux(
+            ViciUMAMux(serial_port1),
+            ViciUMAMux(serial_port2),
+        )
 
     def home(self, unit_id=None):
         self.cmux_gpio.home()
--- system/device/device_init.py
+++ system/device/device_init.py
@@ -115,26 +115,8 @@ def init_engine():
     if DEVICE == Device.MUX:
         from gasera.acquisition.mux import MuxAcquisitionEngine
         from gasera.motion.mux_motion import MuxMotion
-        from system.mux.mux_gpio import GPIOMux
-        from system.mux.mux_vici_uma import ViciUMAMux
-        from system.mux.cascaded_mux import CascadedMux
-        from system.gpio import pin_assignments as PINS
-        
-        serial_port1 = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A90KFA3G-if00-port0"
-        serial_port2 = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A9C7BUGI-if00-port0"
 
-        cmux_gpio = CascadedMux(
-            GPIOMux(home_pin=PINS.OC5_PIN, next_pin=PINS.OC4_PIN),
-            GPIOMux(home_pin=PINS.OC2_PIN, next_pin=PINS.OC1_PIN),
-        )
-
-        cmux_serial = CascadedMux(
-            ViciUMAMux(serial_port1),
-            ViciUMAMux(serial_port2),
-        )
-
-        motion = MuxMotion(cmux_gpio=cmux_gpio, cmux_serial=cmux_serial)
-
+        motion = MuxMotion()
         services.engine_service = MuxAcquisitionEngine(motion)
     elif DEVICE == Device.MOTOR:
         from gasera.acquisition.motor import MotorAcquisitionEngine
